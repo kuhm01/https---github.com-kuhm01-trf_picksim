3fffbeda88a78e3b649644b230455b3ae079eb2a
kuhm01 <kuhm01@naver.com>
2023-04-08 00:44:13 +0900

    implementing button event

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import useStore from './store/store.js'
 
 function App() {
+
+  const { count, increaseCount, removeCount } = useStore(state => state);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {count}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={increaseCount}>increase</button>
+        <button onClick={removeCount}>remove</button>
       </header>
     </div>
   );
